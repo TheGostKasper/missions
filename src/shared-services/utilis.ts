75aca3e8a4f612d1c6ccb7eab8458da083af9805
TheGostKasper <thegostkasper@gmail.com>
2022-03-26 01:12:06 +0200

    update title page to have the last elem  in the page

--- src/shared-services/utilis.ts
+++ src/shared-services/utilis.ts
@@ -43,20 +43,21 @@ export class Utilis {
     return groupedItems;
   };
 
-  static updateMetaTags(mission: IMission | undefined) {
-    const changeMeta = (prop: string, value: string) => {
-      document
-        .querySelector(`meta[property="og:${prop}"]`)
-        ?.setAttribute("content", value);
-      document
-        .querySelector(`meta[property="twitter:${prop}"]`)
-        ?.setAttribute("content", value);
-    };
+  static changeMeta = (prop: string, value: string) => {
+    document
+      .querySelector(`meta[property="og:${prop}"]`)
+      ?.setAttribute("content", value);
+    document
+      .querySelector(`meta[property="twitter:${prop}"]`)
+      ?.setAttribute("content", value);
+  };
 
+  static updateMetaTags(mission: IMission | undefined) {
     if (!!mission) {
-      changeMeta("title", mission.title);
-      changeMeta("image", mission.image?.src || "");
-      changeMeta("video", mission.video?.src || "");
+      document.title = mission.title;
+      this.changeMeta("title", mission.title);
+      this.changeMeta("image", mission.image?.src || "");
+      this.changeMeta("video", mission.video?.src || "");
     }
   }
 }
